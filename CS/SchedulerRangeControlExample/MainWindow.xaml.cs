6d40cd4460909334433011d9e091b7038132e8cb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SchedulerRangeControlExample/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.XtraScheduler;
+using System;
+
+namespace SchedulerRangeControlExample
+{
+    public partial class MainWindow : DXWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void schedulerControl1_RangeControlAutoAdjusting(object sender,
+                                                              RangeControlAdjustEventArgs e)
+        {
+            SchedulerViewType activeViewType = scheduler.ActiveViewType;
+
+            if (activeViewType == SchedulerViewType.WorkWeek)
+            {
+                e.Scales[4].DisplayFormat = "dddd";
+                e.Scales[4].Width = 70;
+            }
+
+            if (activeViewType == SchedulerViewType.Month)
+            {
+                e.Scales.Clear();
+
+                TimeScaleMonth monthScale = new TimeScaleMonth();
+                monthScale.DisplayFormat = "MMMM yyyy";
+                e.Scales.Add(monthScale);
+
+                TwoWeekTimeScale twoWeekTimeScale = new TwoWeekTimeScale();
+                twoWeekTimeScale.Width = 120;
+                e.Scales.Add(twoWeekTimeScale);
+
+                e.RangeMinimum = new DateTime(e.RangeMinimum.Year, 1, 1);
+                e.RangeMaximum = e.RangeMinimum.AddYears(1);
+            }
+        }
+    }
+
+    public class TwoWeekTimeScale : TimeScaleFixedInterval
+    {
+        public TwoWeekTimeScale()
+            : base(TimeSpan.FromDays(14))
+        {
+        }
+        public override DateTime Floor(DateTime date)
+        {
+            DateTime startOfWeeek = DevExpress.XtraScheduler.Native.DateTimeHelper.GetStartOfWeekUI(date,
+                                                                    DevExpress.XtraScheduler.Native.DateTimeHelper.FirstDayOfWeek);
+            if (DevExpress.XtraScheduler.Native.DateTimeHelper.GetWeekOfYear(date) % 2 == 0)
+                return startOfWeeek.AddDays(-7);
+
+            return startOfWeeek;
+        }
+        public override string FormatCaption(DateTime start, DateTime end)
+        {
+            string dateString = "Week {0} - Week {1}";
+            return String.Format(dateString, DevExpress.XtraScheduler.Native.DateTimeHelper.GetWeekOfYear(start),
+                                 DevExpress.XtraScheduler.Native.DateTimeHelper.GetWeekOfYear(end.AddTicks(-1)));
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to customize the RangeControl auto adjustment behavior
+
+
+<p>This example demonstrates how to specify visible scales and available range that should be set for the RangeControl integrated with the SchedulerControl when an end-user switches the scheduler view to <strong>Work</strong><strong>Week</strong> view or <strong>Month</strong><strong> </strong>view. To do this, set the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerOptionsRangeControl_AutoAdjustModetopic"><u>SchedulerOptionsRangeControl.AutoAdjustMode</u></a> option to <strong>true</strong> and handle the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_RangeControlAutoAdjustingtopic"><u>SchedulerControl.RangeControlAutoAdjusting</u></a> event.</p><br />
+
+
+<br/>
+
+
